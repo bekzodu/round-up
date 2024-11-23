731ebfacc71d82bcb136318e95537d1db75bba4f
Bekzod <begzod0320@gmail.com>
2024-11-23 15:29:02 -0600

    implemeneted firebase connection

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import { app } from './firebase/config';
 
 function App() {
+  console.log('Firebase initialized:', app);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Dark Mode Version
         </p>
         <a
           className="App-link"
